6b679e5054cd6613540828c9c54a5468687de637
juwel119 <juwelcse18@gmail.com>
2022-09-16 12:46:44 +0600

    first commit

---
+++ myapp/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from.import views
+urlpatterns=[
+path('',views.dashboard,name='Dashboard'),
+path('home/',views.home,name='Home'),
+path('blog/',views.blog,name='Blog'),
+path('about/',views.about,name='About'),
+path('contract',views.contract,name='Contract'),
+]
---
+++ myapp/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def dashboard(request):
+    return render(request,'home.html')
+def home(request):
+    return render(request,'home.html')
+def blog(request):
+    return render(request,'blog.html')
+def about(request):
+     return render(request,'about.html')
+def contract(request):
+     return render(request,'contract.html')
